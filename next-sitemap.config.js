41d293731f50f7a5034e4e7d88ad771a9d8319e8
wonnie <jaewon@wonnie-MacBook-Pro.local>
2025-07-08 16:29:59 +0900

    ”update_xml_2”

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://naviahub.dev',
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    exclude: ['/sitemap.xml'],
-  };
